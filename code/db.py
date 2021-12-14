1557f65452639fba482bdee6636cc0208e3bedb0
Alex <gassemzachary@gmail.com>
2021-12-14 14:45:33 +0000

    Neatened up executemany() parts with helper function

--- code/db.py
+++ code/db.py
@@ -6,7 +6,7 @@ DB_NAME = "database.db"
 choiceSql = "INSERT INTO choices(choice_id, text, index_num) VALUES (?, ?, ?)"
 questionSql = "INSERT INTO questions(question_id, text, index_num, \
 max_answers) VALUES (?, ?, ?, ?)"
-questionChoiceSql = "INSERT INTO question_choices(choice_id, question_id)\
+questionChoiceSql = "INSERT INTO question_choices(question_id, choice_id)\
 VALUES (?, ?)"
 electionSql = "INSERT INTO elections(election_id, start_time, end_time)\
 VALUES (?, ?, ?)"
@@ -22,6 +22,13 @@ whatever reason we are unsuccessful then print the error message and return None
     except Error as e:
         print(e)
         return None
+
+def mapTups(sql_list, index, constant):
+    """Helper function to go from [(..., val_0,...), (..., val_1,...)] and some
+CONSTANT to -> [(CONSTANT, val_0), (CONSTANT, val_1), ...] which is better
+for Cursor.executemany()"""
+    return list(map(lambda tups, val: (val, tups[index]),
+                    sql_list, [constant]*len(sql_list)))
 
 def insertElection(election):
     """Takes an Election object, inserts all of its Questions, Choices and other
@@ -41,21 +48,17 @@ data into the database. Returns None if we encounter an error -- True otherwise.
         # insert all the questions for this election into the 'questions' table
         sql_questions = election.getSqlQuestions()
         cur.executemany(questionSql, sql_questions)
+
+        # link all the questions with this given election
+        cur.executemany(electionQuestionSql, mapTups(sql_questions,
+                                                     index=0, constant=eID))
         for q_index, question in questions:
             # insert all the choices for this question into the 'choices' table
-            sql_choices = question.getSqlChoices()
-            cur.executemany(choiceSql, sql_choices)
+            cur.executemany(choiceSql, question.getSqlChoices())
 
             # link all the choices with this given question
-            qID = question.getQuestionId()
-            # go from [(choice_id_0,...), (choice_id_1,...)], question_id
-            # to -> [(choice_id_0, question_id), (choice_id_1, question_id), ...]
-            questionChoices = list(map(lambda tups, ids: (tups[0], ids),
-                                       sql_choices, [qID]*len(xList)))
-            cur.executemany(questionChoiceSql, questionChoices)
-                
-            # link this question to the election
-            cur.execute(electionQuestionSql, (eID, qID))
+            cur.executemany(questionChoiceSql, mapTups(sql_choices, index=0,
+                                                       constant=question.getQuestionId()))
         con.commit()
         con.close()
         return True
